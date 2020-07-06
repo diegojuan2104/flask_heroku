010e77a392531c884efd26e23fd259785457eb95
diegojuan2104 <diegojuan2104@gmail.com>
2020-07-06 17:06:19 -0500

    added pgSQL

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
+import os
 
 from security import authenticate, identity
 from resources.user import UserRegister
@@ -8,12 +9,14 @@ from resources.item import Item, ItemList
 from resources.store import Store,StoreList
 
 app = Flask(__name__) #This means that this it's the main doc where flask runs
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db' # To locate the db doc, and add sqlite,postgreSQL,Oracle whatever....
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(('DATABASE_URL')','sqlite:///data.db') # To locate the db doc, and add sqlite,postgreSQL,Oracle whatever....
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 api = Api(app) #Allow add Resources from flask Rest-full
 app.secret_key = '123456789123456789qwewqqwer'
 
-
+@app.before_first_request #To Create all the tables before run 
+def create_tables():
+    db.create_all()
 
 jwt = JWT(app, authenticate, identity) # using URL/auth whit authenticate comfirm the user and password, and with identity returns the user token encripted 
 
